47c4a3dca3c951005ca8e4aeeabbd1d99559872e
Fernando Coutinho <fa2c1@hotmail.com>
2022-04-05 21:01:21 -0300

    Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
+import './styles.css';
+
 function Navbar(){
-    return(
-        <div>
-            <h1>Navbar</h1>
-        </div>
+    return (
+        <nav className="navbar main-nav">
+            <Link to="/" className="nav-logo-text">
+                <h4 className="text-primary">Github API</h4>
+            </Link>
+        </nav>
     );
 }
 
