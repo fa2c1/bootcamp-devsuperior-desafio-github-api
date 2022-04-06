9e21d55452083a8205b74c320f70395b1c0d23fb
Fernando Coutinho <fa2c1@hotmail.com>
2022-04-05 21:41:38 -0300

    Page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
 function Home(){
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="home-container">
+            <h1 className='txt-home-h1'>Desafio Github API</h1>
+            <h3 className='txt-home-h3'>Bootcamp Spring React - DevSuperior</h3>
+            <Link to="/">
+                <button className="btn btn-lg start-button">Começar</button>
+            </Link>
         </div>
     );
 }
